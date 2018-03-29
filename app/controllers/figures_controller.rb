b291326a042deb9d55a751584088de0b8e84d26d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-29 18:12:23 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,11 +14,11 @@ class FiguresController < ApplicationController
   #   if !params[:landmark][:name].empty?
   #     @figure.landmarks << Landmark.create(params[:landmark])
   #   end
-  # 
+  #
   #   if !params[:title][:name].empty?
   #     @figure.titles << Title.create(params[:title])
   #   end
-  # 
+  #
   #   @figure.save
   #   redirect to "figures/#{@figure.id}"
   # end
